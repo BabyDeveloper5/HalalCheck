dbae250faebeabbfa8b78c806f53aeeb6179e69f
sinbad <sinbad2015@gmail.com>
2025-08-03 19:00:52 +0200

    Added a dev-only flag to show/hide some UI elements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,12 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("boolean", "SHOW_DEV_CONTROLS", "true")
+        }
         release {
-            isMinifyEnabled = false
+            buildConfigField("boolean", "SHOW_DEV_CONTROLS", "false")
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -106,16 +110,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.04.01"))
     implementation("androidx.compose.ui:ui-graphics")
 
-/*
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-
-    implementation("androidx.compose.ui:ui")
-
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    */
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
